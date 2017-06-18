bb1ac4b6467fe5091036cf434f0335919c7469db
TakkuMattsu <Takku.Mattsu@gmail.com>
2017-06-18 22:25:36 +0900

    Add Project

---
+++ CrashlyticsLogSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CrashlyticsLogSample
+//
+//  Created by t_matsumura on 2017/06/18.
+//  Copyright © 2017年 TakkuMattsu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
